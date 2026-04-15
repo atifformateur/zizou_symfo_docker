abc12e37761aaa0920b860acdab97ea12ca82c34
atifformateur <atif.zourgani@laplateforme.io>
2026-04-15 16:09:47 +0200

    first commit

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Player;
+use App\Form\PlayerType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,23 +24,15 @@ final class PlayerController extends AbstractController
     #[Route('/player/new', name: 'player_new', methods:['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $em)
     {
-       if ($request->isMethod('POST')) {
-            //creation d'une instance de player
-            $newPlayer = new Player();
+        $newPlayer = new Player();
 
-            //je remplis mon instance avec les données du form
-            $newPlayer
-                ->setNom($request->request->get('nom'))
-                ->setUsername($request->request->get('username'))
-                ->setNumber($request->request->get('number'));
-
-            //envoyer en bdd
-            $em->persist($newPlayer);
-            $em->flush();
-
-            return $this->redirectToRoute('player_index');
-        }
-        return $this->render('player/new.html.twig');
+        $formPlayer = $this->createForm(PlayerType::class, $newPlayer);
+        // $form->handleRequest($request);
+        
+       
+        return $this->render('player/new.html.twig', [
+            'formPlayer' => $formPlayer
+        ]);
     }
 
     
